edc31c0272e97474d527ea27a19c3a5ac4effb75
Jayce S. Chant <jayce.chant@gmail.com>
2020-07-11 00:36:53 +0800

    improve the solutions of #1504

--- leetcode/1504/5454.go
+++ leetcode/1504/5454.go
@@ -6,21 +6,27 @@ package main
 // 0 <= mat[i][j] <= 1
 func numSubmat(mat [][]int) int {
 	count := 0
-	row := len(mat)
-	col := len(mat[0])
+	rowNum := len(mat)
+	colNum := len(mat[0])
 
-	mymat := make([][]int, row)
-	for i := 0; i < row; i++ {
-		mymat[i] = make([]int, col)
-		copy(mymat[i], mat[i])
-	}
+	mymat := make([]int, colNum)
 	// fmt.Println(mymat)
 
-	for height := 1; height <= row; height++ {
-		for r := 0; r <= row-height; r++ {
+	for rowSt := 0; rowSt < rowNum; rowSt++ {
+		for rowEd := rowSt; rowEd < rowNum; rowEd++ {
+			if rowEd == rowSt {
+				copy(mymat, mat[rowSt])
+			} else {
+				for col := 0; col < colNum; col++ {
+					if mat[rowEd][col] == 0 {
+						mymat[col] = 0
+					}
+				}
+			}
+
 			length := 0
-			for c := 0; c < col; c++ {
-				if mymat[r][c] == 1 {
+			for col := 0; col < colNum; col++ {
+				if mymat[col] == 1 {
 					length++
 					count += length
 				} else {
@@ -28,17 +34,111 @@ func numSubmat(mat [][]int) int {
 				}
 			}
 		}
+	}
+	return count
+}
 
-		// compress
-		for r := 0; r < row-height; r++ {
-			for c := 0; c < col; c++ {
-				if mymat[r][c] == 1 && mat[r+height][c] == 1 {
-					mymat[r][c] = 1
+// 84 ms, faster than 23.53% (min 16ms)
+// 6 MB, less than 100.00%
+func numSubmat2(mat [][]int) int {
+	count := 0
+	rowNum := len(mat)
+	colNum := len(mat[0])
+
+	mymat := make([]bool, colNum)
+
+	for rowSt := 0; rowSt < rowNum; rowSt++ {
+		for col := 0; col < colNum; col++ {
+			mymat[col] = true
+		}
+		for rowEd := rowSt; rowEd < rowNum; rowEd++ {
+			length := 0
+			for col := 0; col < colNum; col++ {
+				if mat[rowEd][col] == 0 {
+					mymat[col] = false
+				}
+				if mymat[col] {
+					length++
+					count += length
 				} else {
-					mymat[r][c] = 0
+					length = 0
 				}
 			}
 		}
 	}
 	return count
+}
+
+// 28 ms, faster than 79.41%
+// 6.1 MB, less than 100.00%
+func numSubmat3(mat [][]int) int {
+	count := 0
+	rowNum := len(mat)
+	colNum := len(mat[0])
+
+	height := make([]int, colNum)
+	sum := make([]int, colNum)
+	stck := newStack()
+	for row := 0; row < rowNum; row++ {
+		for col := 0; col < colNum; col++ {
+			if mat[row][col] == 1 {
+				height[col]++
+			} else {
+				height[col] = 0
+			}
+			sum[col] = 0
+		}
+
+		stck.clear()
+
+		for col := 0; col < colNum; col++ {
+			for stck.size() > 0 && height[stck.top()] >= height[col] {
+				stck.pop()
+			}
+
+			if stck.size() == 0 {
+				sum[col] = height[col] * (col + 1)
+			} else {
+				preCol := stck.top()
+				sum[col] = sum[preCol] + height[col]*(col-preCol)
+			}
+
+			stck.push(col)
+		}
+
+		for col := 0; col < colNum; col++ {
+			count += sum[col]
+		}
+	}
+	return count
+}
+
+type stack struct {
+	data []int
+}
+
+func newStack() *stack {
+	return &stack{
+		data: make([]int, 0),
+	}
+}
+
+func (s *stack) size() int {
+	return len(s.data)
+}
+
+func (s *stack) push(v int) {
+	s.data = append(s.data, v)
+}
+
+func (s *stack) top() int {
+	return s.data[len(s.data)-1]
+}
+
+func (s *stack) pop() {
+	s.data = s.data[:len(s.data)-1]
+}
+
+func (s *stack) clear() {
+	s.data = s.data[:0]
 }
