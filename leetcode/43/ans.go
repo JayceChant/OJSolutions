68850e489d023eb9ccf1ccbd6dd241f0beaf24d5
Jayce S. Chant <jayce.chant@gmail.com>
2020-07-28 13:05:31 +0800

    leetcode/43 additional solution without converting to integer
    It seems I misunderstood the question.
    Not only can't you use libraries (for big-int or str-conv),
    you can't even convert to integers on your own.

--- leetcode/43/ans.go
+++ leetcode/43/ans.go
@@ -7,7 +7,7 @@ import (
 
 // 0ms, 100%
 // 2.4MB, 53.33%
-func multiply(str1 string, str2 string) string {
+func multiplyInt(str1 string, str2 string) string {
 	if str1 == "0" || str2 == "0" {
 		return "0"
 	}
@@ -126,4 +126,43 @@ func parse32(str string, length int) []uint32 {
 		start -= 9
 	}
 	return nums
+}
+
+// It seems I misunderstood the question.
+// Not only can't you use libraries (for big-int or str-conv),
+// you can't even convert to integers on your own.
+// 0ms, 100%
+// 2.3MB, 76.92%
+func multiply(num1 string, num2 string) string {
+	if num1 == "0" || num2 == "0" {
+		return "0"
+	}
+
+	len1 := len(num1)
+	len2 := len(num2)
+	lenRes := len1 + len2
+	res := make([]byte, lenRes)
+
+	for i1 := len1 - 1; i1 >= 0; i1-- {
+		n1 := num1[i1] - '0'
+		for i2 := len2 - 1; i2 >= 0; i2-- {
+			i := i1 + i2 + 1
+			d := res[i] + n1*(num2[i2]-'0')
+			for d >= 10 {
+				res[i] = d % 10
+				i--
+				d = res[i] + d/10
+			}
+			res[i] = d
+		}
+	}
+
+	for i := lenRes - 1; i >= 0; i-- {
+		res[i] += '0'
+	}
+	i := 0
+	if res[i] == '0' {
+		i++
+	}
+	return string(res[i:])
 }
